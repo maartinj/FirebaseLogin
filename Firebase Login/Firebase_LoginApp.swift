ad0e3b8eb95c879691bb7b77421a354612acdcfc
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-01-02 18:27:30 +0100

    Initial Commit

---
+++ Firebase Login/Firebase_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_LoginApp.swift
+//  Firebase Login
+//
+//  Created by Marcin Jędrzejak on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
